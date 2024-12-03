24979f24fbc81e5e43044e997fd0d2d1e2de48f6
Jorge Afonso <128873434+MOR4Xx@users.noreply.github.com>
2024-12-02 21:19:06 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-window.addEventListener('scroll', function() {
-  const header = document.getElementById('header');
-  if (window.scrollY > document.querySelector('.hero').offsetHeight - header.offsetHeight) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
+  const apresentacaoSection = document.querySelector('#apresentacao');
+
+  window.addEventListener('scroll', () => {
+    const sectionTop = apresentacaoSection.getBoundingClientRect().top;
+    const sectionBottom = apresentacaoSection.getBoundingClientRect().bottom;
+    const viewportHeight = window.innerHeight;
+
+    if (sectionTop < viewportHeight && sectionBottom > 0) {
+      header.style.opacity = '0';
+      header.style.pointerEvents = 'none';
+    } else {
+      header.style.opacity = '1';
+      header.style.pointerEvents = 'auto';
+    }
+  });
 });
